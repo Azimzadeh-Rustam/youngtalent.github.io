514be1c6dd539d02a4a2cfefdaa098a44c0fd9ca
Rustam <125408774+Azimzadeh-Rustam@users.noreply.github.com>
2023-09-16 22:33:21 +0400

    Delete issues/issues_years.php

--- issues/issues_years.php
+++
@@ -1,8 +0,0 @@
-<ul>
-	<!--
-	<li><a href="issues_2024.php">2024</a></li>
-	<li><a href="issues_2023.php">2023</a></li>
-	-->
-	<li><a href="issues_2022.php">2022</a></li>
-	<li><a href="issues_2021.php">2021</a></li>
-</ul>
